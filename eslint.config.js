88716158dfa17806d0880d1ad17c43db2fa5ac54
ngthtnhung <ngthtnhung2005@gmail.com>
2026-04-27 15:33:43 +0700

    Replace .eslintrc.json with eslint.config.js (flat config)

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+// ESLint configuration migrated to the new flat config format for ESLint v9+
+/** @type {import('eslint').Linter.FlatConfig} */
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+      globals: {
+        ...require('eslint/conf/environments').node.globals,
+        ...require('eslint/conf/environments').es2021.globals,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+  },
+  {
+    ignores: ['node_modules/**'],
+  },
+];
