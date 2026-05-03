e2957bf3684819cd3d103f3e867d881b84051dd9
Huỳnh Nhật Tiến <huynhnhattien0411@gmail.com>
2026-05-03 19:43:36 +0700

    feat: complete monitoring stack with custom metrics and alerts

--- main.js
+++ main.js
@@ -8,13 +8,45 @@ const uiRoutes = require('./routes/uiRoutes');
 const path = require('path');
 const fs = require('fs'); 
 
+// --- BẮT ĐẦU: Cấu hình Prometheus Custom Metrics ---
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+// Thu thập các metrics mặc định (CPU, RAM của Node.js)
+collectDefaultMetrics({ register: client.register });
+
+// Đo lường thời gian và số lượng HTTP Request
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10]
+});
+// --- KẾT THÚC: Cấu hình Prometheus ---
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// --- BẮT ĐẦU: Middleware ghi nhận Metrics cho mỗi request ---
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ route: req.route ? req.route.path : req.path, code: res.statusCode, method: req.method });
+  });
+  next();
+});
+// --- KẾT THÚC: Middleware ---
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+// --- BẮT ĐẦU: Endpoint xuất Metrics cho Prometheus ---
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+// --- KẾT THÚC: Endpoint ---
 
 app.use('/', uiRoutes);
 app.use('/products', productRoutes);
